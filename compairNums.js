5b3832617378a18032823e9dd9b5a700b3dd768f
john-reiner <johnreiner1990@gmail.com>
2020-11-25 15:33:26 -0500

    setting up compNums problem

---
+++ compairNums.js
@@ -0,0 +1,6 @@
+// given 2 numbers, compair how close the two numbers are in simular digits 
+// for example 1234567890 and 1234567891 should return 90% as 90% of the digits compaired are the same
+
+const compairNums = (num1, num2) => {
+    
+}
