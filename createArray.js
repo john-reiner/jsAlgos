9d8d17ff20501765c9133f86fa7e2324278406c9
john-reiner <johnreiner1990@gmail.com>
2020-12-03 09:21:22 -0500

    delete and shitItems methods

--- createArray.js
+++ createArray.js
@@ -20,13 +20,26 @@ class MyArray {
         this.length --
         return lastItem
     }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index)
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i+1]
+        }
+        delete this.data[this.length-1]
+        this.length --
+    }
 }
 
 const newArray = new MyArray()
 newArray.push('hi')
 newArray.push('john')
 newArray.push('!')
-
+newArray.delete(1)
 console.log(newArray)
-newArray.pop()
+// newArray.pop()
 console.log(newArray)
