edc08475428d722d2628c2be5f6af2a9e4601e61
john-reiner <johnreiner1990@gmail.com>
2020-12-11 10:19:34 -0500

    video solution with checks and itteration to who time complexity of a hash

--- hash_tables/implementHashTable.js
+++ hash_tables/implementHashTable.js
@@ -13,14 +13,29 @@ class HashTable {
     }
 
     set(key, value) {
-        this.data[this._hash(key)] = value
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = []
+        }
+        this.data[address].push([key, value])
+        return this.data
     }
 
     get(key) {
-        return this.data[this._hash(key)]
+        let address = this._hash(key);
+        const currentBucket = this.data[address]
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1]
+                }
+            }
+        }
+        return undefined
     }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('John Reiner', 30)
-console.log(myHashTable.get('John Reiner'))
+myHashTable.set('David Tendler', 31)
+console.log(myHashTable.get('David Tendler'))
